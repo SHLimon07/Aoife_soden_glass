bb419e9c0ce9eca540edf7a41b2bf6bf10a2e179
SHLimon07 <sajjadblanco07@gmail.com>
2020-06-25 23:27:06 +0600

    navBar created

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function navBarToggle() {
+    var icon = document.querySelector('.buttonDiv');
+    var menu = document.querySelector('.collaps');
+
+    icon.classList.toggle('toggle');
+    menu.classList.toggle('toggle');
+}
