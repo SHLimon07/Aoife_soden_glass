e320861345f11a8227c309a1601e2a1932d08212
SHLimon07 <sajjadblanco07@gmail.com>
2020-07-07 18:27:48 +0600

    All designs done

--- js/homepage.js
+++ js/homepage.js
@@ -1,9 +1,20 @@
+// Getting the elements from HTML
+const carousel = document.getElementById('carousel');
+const size = carousel.children.length;
+var carouselCounter;
+
 homeMain();
 
 async function homeMain() {
     var bannerImg = document.getElementById('banner').querySelectorAll('.images');
     var changeDelay = 5000;
     setInterval(autoplay, bannerImg.length * changeDelay, bannerImg, changeDelay);
+
+    //since initialy the slider is on the postion of the last clone
+    gotoFirst();
+
+    //starting the autoplay 
+    setInterval(autoPlay, 5000);
 }
 
 async function autoplay(bannerImg, delay) {
@@ -23,4 +34,67 @@ function setOpacity(now, prev, delay) {
             resolve()
         }, delay)
     })
+}
+
+function autoPlay()
+{
+    //continiously move to the next slide
+    gotoNext();
+}
+
+function gotoFirst()
+{
+    //takes the slider straight to the first image, without any sliding effect to create the loop
+    carouselCounter=1;
+    carousel.classList.add('noTransition');
+    carousel.style.transform = 'translateX(-100%)';
+}
+
+function gotoLast()
+{
+    //takes the slider straight to the last image, without any sliding effect to create the loop
+    carouselCounter=size-2;
+    carousel.classList.add('noTransition');
+    carousel.style.transform += 'translateX(-' + (size-2)*100 + '%)';
+}
+
+
+function gotoNext() {
+    // Does the transition to the next slide
+    if(carouselCounter>=size-1)//this condition is for handing errors of counter
+        return;
+
+    //the class that was added by gotofirst()/gotolast()
+    carousel.classList.remove('noTransition');
+    carouselCounter++;
+    carousel.style.transform = 'translateX(-' + carouselCounter*100 + '%)';
+    if(carouselCounter==size-1)checkIf();//checking if the current slide is a clone one
+}
+
+function gotoPrev() {
+    // Does the transition to the next slide
+
+    if(carouselCounter<=0)//this condition is for handing errors of counter
+        return;
+
+    //the class that was added by gotofirst()/gotolast()
+    carousel.classList.remove('noTransition');
+    carouselCounter--;
+    carousel.style.transform = 'translateX(-' + carouselCounter*100 + '%)';
+    if(carouselCounter==0)checkIf();//checking if the current slide is a clone one
+}
+
+function checkIf()
+{
+    carousel.addEventListener('transitionend',function()
+    {
+        if(carousel.children[carouselCounter].id=="firstClone")
+        {
+            gotoFirst();//to create the loop effect
+        }
+        else if(carousel.children[carouselCounter].id=="lastClone")
+        {
+            gotoLast();//to create the loop effect
+        }
+    });
 }
--- js/main.js
+++ js/main.js
@@ -5,3 +5,22 @@ function navBarToggle() {
     icon.classList.toggle('toggle');
     menu.classList.toggle('toggle');
 }
+
+main();
+
+async function main()
+{
+	
+}
+
+async function fetching (type) {
+	//this function fetches the api from google sheet as json 
+
+	if(type==null)
+		type='';
+	else
+		type = 'wp/v2/'+type;
+	const response = await fetch('http://sajjad.jprkopat.com/semester2/glassart/wp-json/'+type);
+	var dataSheet = await response.json();
+	return dataSheet;
+}
