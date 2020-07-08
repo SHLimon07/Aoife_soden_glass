f3b61c1a5f54c07ab81bd38590b9a02a4e49ad35
SHLimon07 <sajjadblanco07@gmail.com>
2020-07-08 13:13:51 +0600

    cart section done

--- js/cart.js
+++ js/cart.js
@@ -1,27 +1,56 @@
 
-var shopUrl = "posts?categories=4";
+cartMain();
 
-artShopMain();
-
-async function artShopMain() {
+async function cartMain() {
 
     //getting the art shop posts
-    var shopData = await fetching(shopUrl);
+    var cartData = getCartData();
+
+    if(cartData.length==0)
+        empty();
 
     //initializing the data to hmtl
-    initContent(shopData);
+    initContent(cartData);
 
 }
 
 
-function initContent (shopData) {
+function empty () {
+    // body... 
+    var no = document.createElement('h1');
+    no.innerHTML = 'No Resutls';
+    no.style.textAlign = 'center';
+    no.style.color = '#d8d8d8';  
+    document.querySelector('.productDivs').appendChild(no);
+}
+
+function getCartData () {
     // body... 
 
-    var sz = shopData.length;
+    var inCart = localStorage.getItem("inCart");
+    inCart = JSON.parse(inCart);
+
+    var cartList = localStorage.getItem("cartList");
+    cartList = JSON.parse(cartList);
+
+    var cartData = [];
+
+    for(i=0;i<cartList.length;i++)
+    {
+        cartData.push(inCart[cartList[i]]);
+    }
+
+    return cartData;
+}
+
+function initContent (cartData) {
+    // body... 
+
+    var sz = cartData.length;
     var mainDiv = document.querySelector('.productDivs');
     for(var i=0;i<sz;i++)
     {
-        createItem(shopData[i],mainDiv);
+        createItem(cartData[i],mainDiv);
     }
 }
 
@@ -55,6 +84,12 @@ function createItem (data,div) {
     var price = document.createElement('span');
     price.innerHTML = getContent(data).querySelector('.price').outerHTML;
 
+    var quantityDiv = document.createElement('div');
+    quantityDiv.classList.add('quantity');
+
+    var quantity = document.createElement('span');
+    quantity.innerHTML = "Quantity: " + data.cart;
+
     var textDiv = document.createElement('div');
     textDiv.classList.add('text');
 
@@ -70,6 +105,8 @@ function createItem (data,div) {
     titleDiv.appendChild(title);
     contentDiv.appendChild(priceDiv);
     priceDiv.appendChild(price);
+    contentDiv.appendChild(quantityDiv);
+    quantityDiv.appendChild(quantity);
     contentDiv.appendChild(textDiv);
     textDiv.appendChild(text);
 }
--- js/shopItem.js
+++ js/shopItem.js
@@ -1,17 +1,86 @@
+var inCart;
+var itemId;
+var itemData;
+var cartList;
 
+shopItemMain();
 
-latestNewsMain();
+async function shopItemMain() {
 
-async function latestNewsMain() {
-
-    //getting the id of the news post
-    var itemId = getId();
+    //getting the id of the item post
+    itemId = getId();
 
     //getting the data of that post from cms
-    var itemData = await fetching('posts/'+itemId);
+    itemData = await fetching('posts/'+itemId);
 
     initHTML(itemData);
 
+    //getting the object from local storage
+    initCart();
+
+}
+
+function initCart () {
+    // body... 
+    inCart = localStorage.getItem("inCart");
+    inCart = JSON.parse(inCart);
+
+    cartList = localStorage.getItem("cartList");
+    cartList = JSON.parse(cartList);
+
+    if(inCart == undefined)
+    {
+        localStorage.setItem("inCart", "{}");
+        inCart = localStorage.getItem("inCart");
+        inCart = JSON.parse(inCart);
+    }
+
+    if(cartList == undefined)
+    {
+        localStorage.setItem("cartList", "[]");
+        cartList = localStorage.getItem("cartList");
+        cartList = JSON.parse(cartList);
+    }
+}
+
+function change (num) {
+    // body...
+    var now = document.querySelector('.quantity').innerHTML;
+    now = new Number(now);
+
+    now += num;
+    if(now>0)
+        document.querySelector('.quantity').innerHTML = now; 
+}
+
+function addToCart () {
+    // body... 
+
+    var quantity = document.querySelector('.addToCart .quantity').innerHTML;
+    quantity = new Number(quantity);
+
+    if(inCart[itemId]==undefined)
+    {
+        cartList.push(itemId);
+        itemData.cart=quantity;
+    }
+    else 
+    {
+        itemData.cart= new Number(inCart[itemId].cart) + quantity ;
+    }
+    itemData.price = getContent(itemData).querySelector('.price').innerHTML;
+    inCart[itemId]=itemData;
+
+    cartList = JSON.stringify(cartList);
+    localStorage.setItem("cartList", cartList);
+
+    inCart = JSON.stringify(inCart);
+    localStorage.setItem("inCart", inCart);
+
+    inCart = JSON.parse(inCart);
+    cartList = JSON.parse(cartList);
+
+    alert('added ' + quantity + ' ' + itemData.title.rendered + ' to cart');
 }
 
 function getId () {
